3e27bfc5f0299fe8b8bfb2b0617e249d66ad28cd
emexkayyy <krishnan.megha18@gmail.com>
2026-04-27 17:08:59 -0700

    hi big update addededed hoome

---
+++ loading.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("hoome");
+
+button.addEventListener("click", () => {
+    open("/hoome/hoome.html")
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hello");
+});
--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Adding some logic to change color when hovering over different types of text
+const button = document.getElementById("hoome");
+
+button.addEventListener("click", () => {
+    open("/hoome/hoome.html")
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hello");
+});
